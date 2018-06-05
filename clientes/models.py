825604a09f6d3c9550ad6c14cf72919087bccde6
Paulo Vinícius <pauloviniciusbf@gmail.com>
2018-06-05 14:28:50 -0300

    alterado o campo idade para não ter limite de dígitos

--- clientes/models.py
+++ clientes/models.py
@@ -9,7 +9,7 @@ class Pessoa(models.Model):
     nome_pessoa = models.CharField(max_length=100)
     altura = models.DecimalField(max_digits=3, decimal_places=2)
 #    nascimento = models.DateField(blank=True, null=True)
-    idade = models.IntegerField(max_length=3)
+    idade = models.IntegerField()
 #   endereço = models.CharField(max_length=100, blank=True, null=True)
     cidade = models.CharField(max_length=100)
 #   sexo = models.CharField(max_length=1, choices=SEXO, blank=True, null=True)
