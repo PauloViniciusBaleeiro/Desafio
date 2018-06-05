ac27da295e5d2469027f614a0bc82d6c1de5f293
Paulo Vinícius <pauloviniciusbf@gmail.com>
2018-06-05 15:22:30 -0300

    testes com a app pedido

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from clientes.models import Pessoa
+from restaurantes.models import Restaurante
+from produtos.models import Produto
 
-# Create your models here.
+class Pedido(models.Model):
+    numero = models.AutoField(primary_key=True )
+    pessoas = models.ForeignKey(Pessoa, verbose_name="Lista de clientes cadastrados", on_delete=models.PROTECT)
+    restaurantes = models.ForeignKey(Restaurante, verbose_name="Lista de restautantes cadastrados", on_delete=models.PROTECT)
+    produtos = models.ManyToManyField(Produto, verbose_name="Lista de Produtos")
+    data = models.DateField()
+
+    def __str__(self):
+        return self.numero
