7d98171858f1cef66a8b980ec598d4e3e6147373
Paulo Vinícius <pauloviniciusbf@gmail.com>
2018-06-07 17:15:18 -0300

    Conclusão da pesquisa de Pedidos por pessoa

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from clientes.models import Pessoa
 from .models import Pedido
 from django.http import HttpResponse
 
 
-
 def localiza_pedidos(request, id):
-    pessoa = get_object_or_404(Pessoa, pk=id)
+    pessoa = Pessoa.objects.get(pk=id)
 
     if pessoa is not None:
 
-        lista = Pedido.objects.filter(pessoas=id)
+        #pedidos = Pedido.objects.get(pessoas=id)
+        lista=[]
+        pedidos = Pedido.objects.filter(pessoas=id)
+        for pedido in pedidos:
+            lista.append(pedido)
+        #pedidos = Pedido.objects.all()
+        #return HttpResponse(pessoa.nome_pessoa + " possui " + str(len(pedidos)) + " pedidos registrados")
+        return render(request, 'pedidos.html', {'lista': lista})
+        '''for p in pedidos:
+            return HttpResponse (p.numero)'''
 
-        return HttpResponse(pessoa.nome_pessoa + " possui " + str(len(lista)) + " pedidos registrados")
+    '''else:
 
-    else:
-
-        return HttpResponse("Id informado inválido, tente novamente")
+        return HttpResponse("Id informado inválido, tente novamente")'''
